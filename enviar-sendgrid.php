eb53c96bca417ed01cd332c89913dfbc563b00d3
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-06-24 19:11:08 -0300

    Sendgrid implemented. JavaScript and PHP functions already done

---
+++ enviar-sendgrid.php
@@ -0,0 +1,36 @@
+<?php
+
+require("./sendgrid-php/sendgrid-php.php");
+
+$email_site = "flborreli@gmail.com";
+$nome_site = "Bikcraft";
+
+$email_user = $_POST["email"];
+$nome_user = $_POST["nome"];
+
+$body_content = "";
+foreach( $_POST as $field => $value) {
+  if( $field !== "leaveblank" && $field !== "dontchange" && $field !== "enviar") {
+    $sanitize_value = filter_var($value, FILTER_SANITIZE_STRING);
+    $body_content .= "$field: $value \n";
+  }
+}
+
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom($email_site, $nome_site);
+$email->addTo($email_site, $nome_site);
+
+$email->setReplyTo($email_user, $nome_user);
+
+$email->setSubject("Formulário Bikcraft");
+$email->addContent("text/plain", $body_content);
+
+$sendgrid = new \SendGrid("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");
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo "Caught exception: ". $e->getMessage() ."\n";
+}
