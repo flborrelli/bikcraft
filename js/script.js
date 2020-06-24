581d32288a794ade0a3b5907417339c1d5cac89d
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-06-24 17:07:58 -0300

    Simple slide added with JavaScript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: 'quote', //nome do atributo 
+    auto: true, //se o slide deve passar automatico
+    time: 5000, //tempo de transicao
+  })
+  
+  new SimpleSlide ({
+    slide: 'portfolio',
+    time: 5000,
+    nav: true,
+  })
+}
